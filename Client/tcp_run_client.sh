e6ecf862f9282325ca69831ee092c1cb7b8917bf
Stacey Beard <staceybeard10@gmail.com>
2020-09-26 14:31:54 -0400

    Remove remaining reference to TCPInterface.jar

--- Client/tcp_run_client.sh
+++ Client/tcp_run_client.sh
@@ -1,4 +1,4 @@
-# TODO remove reference to rmi
+# TODO remove reference to rmiobject
 # Usage: ./tcp_run_client.sh [<server_hostname> [<server_rmiobject>]]
 
-java -Djava.security.policy=java.policy -cp ../Server/TCPInterface.jar:. Client.TCPClient $1 $2
+java -Djava.security.policy=java.policy -cp ../Server/RMIInterface.jar:. Client.TCPClient $1 $2
