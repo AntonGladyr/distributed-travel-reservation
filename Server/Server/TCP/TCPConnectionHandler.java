7ec5931fa3441fbadb601edcadd2ce0ac3a54d14
Stacey Beard <staceybeard10@gmail.com>
2020-09-26 22:46:01 -0400

    Print host name and port when accepting a connection on server

--- Server/Server/TCP/TCPConnectionHandler.java
+++ Server/Server/TCP/TCPConnectionHandler.java
@@ -11,9 +11,19 @@ import Server.Interface.TCPMessage;
 public class TCPConnectionHandler implements Runnable {
 	
 	private Socket clientSocket;
-
+	
+	// The remote host name and port attached to the clientSocket
+	private String hostName;
+	private int port;
+	
 	public TCPConnectionHandler(Socket clientSocket) {
 		this.clientSocket = clientSocket;
+		this.hostName = clientSocket.getInetAddress().getHostName();
+		this.port = clientSocket.getPort();
+
+		// Truncate the ".CS.McGill.CA" part of the hostname
+		int indexOfSuffix = this.hostName.indexOf(".CS");
+		if (indexOfSuffix != -1) this.hostName = this.hostName.substring(0, indexOfSuffix);
 	}
 
 	@Override
@@ -48,13 +58,13 @@ public class TCPConnectionHandler implements Runnable {
 	 * Handles messages of type "HELLO"
 	 */
 	private void handleHello() {
-		System.out.println("Received hello message");
+		System.out.println("Received connection request from [" + hostName + ":" + port + "]");
 		try {
 			// Send a hello message in response
 			ObjectOutputStream output = new ObjectOutputStream(clientSocket.getOutputStream());
 			TCPMessage outgoingMessage = new TCPMessage(MessageType.HELLO);
 			output.writeObject(outgoingMessage);
-			System.out.println("Sent hello message in response");
+			System.out.println("Sent a response to accept connection request from [" + hostName + ":" + port + "]");
 		}
 		catch (IOException e) {
 			System.err.println((char)27 + "[31;1mServer exception: " + (char)27 + "[0mFailed to send hello message");
