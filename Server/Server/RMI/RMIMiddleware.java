369079b4cc4799ed4bf53f2b01a8181f7fd4b539
meldunn <46253456+meldunn@users.noreply.github.com>
2020-11-09 00:04:55 -0500

    Update RMIMiddleware.java

--- Server/Server/RMI/RMIMiddleware.java
+++ Server/Server/RMI/RMIMiddleware.java
@@ -32,9 +32,9 @@ public class RMIMiddleware extends Middleware
 			// Bind the remote object's stub in the registry
 			Registry l_registry;
 			try {
-				l_registry = LocateRegistry.createRegistry(33303);
+				l_registry = LocateRegistry.createRegistry(33304);
 			} catch (RemoteException e) {
-				l_registry = LocateRegistry.getRegistry(33303);
+				l_registry = LocateRegistry.getRegistry(33304);
 			}
 			final Registry registry = l_registry;
 			registry.rebind(s_rmiPrefix + s_serverName, resourceManager);
