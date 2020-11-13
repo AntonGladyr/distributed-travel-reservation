ff3ccebd55dbd717760226d08a0105c8e8bf1a1d
meldunn <46253456+meldunn@users.noreply.github.com>
2020-11-13 01:05:19 -0500

    Update ResourceManager.java

--- Server/Server/Common/ResourceManager.java
+++ Server/Server/Common/ResourceManager.java
@@ -425,7 +425,7 @@ public class ResourceManager implements IResourceManager, DataStore {
 	public void shutdown() throws RemoteException{
 		Trace.info("Shutting down " + this.m_name);
 		try {
-			Naming.unbind(m_name);
+			Naming.unbind("group_03_" + m_name);
 		} catch (MalformedURLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
